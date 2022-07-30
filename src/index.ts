88ccb3aa75377c248bfefbc71ad724c703ef332b
xiaoxunyao <huchengyea@163.com>
2022-07-30 13:15:05 +0800

    chore: init

---
+++ src/index.ts
@@ -0,0 +1,47 @@
+import fs from 'fs'
+import {resolve} from 'path'
+import type { ResolvedConfig, PluginOption } from 'vite';
+
+let viteConfig:ResolvedConfig
+
+export default function (): PluginOption{
+  return {
+    name: 'dist-css',
+    apply: 'build',
+    enforce: 'post',
+
+    configResolved (resolvedConfig) {
+      viteConfig = resolvedConfig;
+    },
+
+    writeBundle (option:any, bundle:any) {
+      if (!viteConfig.build || !viteConfig.build.lib) {
+        // only for lib build
+        console.warn('vite-plugin-dist-css only works in lib mode.')
+        return;
+      }
+      if (option.format !== 'es') {
+        // only for es built
+        return;
+      }
+      const files = Object.keys(bundle);
+      const cssFile = files.find((v) => v.endsWith('.css'));
+      if (!cssFile) {
+        return;
+      }
+      for (const file of files) {
+        if (!bundle[file].isEntry) {
+          // only for entry
+          continue;
+        }
+        const outDir = viteConfig.build.outDir || 'dist';
+        const filePath = resolve(viteConfig.root, outDir, file);
+        const data = fs.readFileSync(filePath, {
+          encoding: 'utf8',
+        });
+        fs.writeFileSync(filePath, `import './${cssFile}';\n${data}`);
+      }
+    },
+  };
+}
+
